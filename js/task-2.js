b619b6965be89defdcb0e62427d3dbcd239be9aa
Oleksandr <netesa.sasha@gmail.com>
2024-05-02 00:17:02 +0300

    /1

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+console.log("Task-2");
+function formatMessage(message, maxLength) {
+  let lenght = message.length;
+
+  if (lenght <= maxLength) {
+    return message;
+  }
+  if (lenght > maxLength) {
+    let lenghtBrief = message.slice(0, maxLength);
+    return `${lenghtBrief}...`;
+  }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
